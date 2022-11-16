4500f35a7f63e424ed24f04533e0d7a4da4a2b98
Jake Kim <imjakekim@gmail.com>
2022-11-15 21:12:07 -0800

    first recipe service test

--- spec/services/recipe_service_spec.rb
+++ spec/services/recipe_service_spec.rb
@@ -1,11 +1,9 @@
 require 'rails_helper'
-RSpec.describe VideoService do 
-  context 'class methods' do 
-    describe '#search' do 
-      it 'returns recipes of a given country', :vcr do
-        search = RecipeService.search('france')
-        expect(search).to be_a Hash 
-      end
+RSpec.describe RecipeService do
+  describe '#search' do
+    it 'returns recipes of a given country', :vcr do
+      search = RecipeService.search('france')
+      expect(search).to be_a Hash
     end
   end
 end
