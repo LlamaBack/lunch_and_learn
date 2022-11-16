2e4337106752f4d804ea21cc52e8b86eb68a384c
Jake Kim <imjakekim@gmail.com>
2022-11-16 03:47:08 -0800

    created user serializer and controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,14 @@
 class Api::V1::UsersController < ApplicationController
   def create
+    user = User.new(user_params)
+    user.api_key = SecureRandom.uuid
+    user.save
+    render json: UserSerializer.new(user), status: 201
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email)
   end
 end
