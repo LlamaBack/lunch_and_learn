a04cf7bdf5624563757732b65d935c93208f7a1a
Jake Kim <imjakekim@gmail.com>
2022-11-15 22:10:23 -0800

    added tests for country facade

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -2,6 +2,8 @@ class Api::V1::RecipesController < ApplicationController
   def index
     if params[:country]
       response = RecipeFacade.search(params[:country])
+    else
+      response = RecipeFacade.search(params[CountryFacade.random])
     end
     render json: RecipeSerializer.new(response)
   end
--- spec/facades/recipe_facade_spec.rb
+++ spec/facades/recipe_facade_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe RecipeFacade do
   describe '#search' do
-    it 'creates an array of recipe objects from search results', :vcr do
+    it 'creates an array of recipe objects from a given country', :vcr do
       search_results = RecipeFacade.search('thailand')
       expect(search_results).to be_an Array
       expect(search_results[0]).to be_a Recipe
