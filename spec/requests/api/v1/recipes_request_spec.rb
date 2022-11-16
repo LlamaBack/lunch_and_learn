8241c6946c21eaaf114551b9cf61dd66ea8be24d
Jake Kim <imjakekim@gmail.com>
2022-11-15 22:56:56 -0800

    created recipe happy path endpoint test

---
+++ spec/requests/api/v1/recipes_request_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'Recipes API' do
+  describe 'happy path' do
+    it 'gets a list of recipes from a given country', :vcr do
+      get api_v1_recipes_path({ country: 'thailand' })
+      expect(response).to be_successful
+
+      recipes = JSON.parse(response.body, symbolize_names: true)
+      expect(recipes[:data]).to be_an Array
+
+      recipe1 = recipes[:data][0]
+      expect(recipe1).to have_key :id
+      expect(recipe1[:type]).to eq('recipe')
+      expect(recipe1[:attributes]).to be_a Hash
+      expect(recipe1[:attributes].length).to eq 4
+      expect(recipe1[:attributes].values).to all(be_a String)
+    end
+  end
+end
