f9d9076254b3de8dbe879381f24711fe9bb80119
Jake Kim <imjakekim@gmail.com>
2022-11-16 02:57:31 -0800

    created resource serializer and request endpoint

--- spec/requests/api/v1/learning_resources_request_spec.rb
+++ spec/requests/api/v1/learning_resources_request_spec.rb
@@ -1,20 +1,35 @@
 require 'rails_helper'
-
-RSpec.describe 'Country Recipes endpoint' do
+RSpec.describe 'Country Learning Resources endpoint' do
   describe 'happy path' do
-    it 'gets a list of recipes from a given country', :vcr do
-      get api_v1_recipes_path({ country: 'thailand' })
+    it 'creates a learning resource for a given country', :vcr do
+      get api_v1_learning_resources_path({ country: 'laos' })
       expect(response).to be_successful
 
-      recipes = JSON.parse(response.body, symbolize_names: true)
-      expect(recipes[:data]).to be_an Array
+      learning_resource1 = JSON.parse(response.body, symbolize_names: true)
+      expect(learning_resource1).to be_a Hash
 
-      recipe1 = recipes[:data][0]
-      expect(recipe1).to have_key :id
-      expect(recipe1[:type]).to eq('recipe')
-      expect(recipe1[:attributes]).to be_a Hash
-      expect(recipe1[:attributes].length).to eq 4
-      expect(recipe1[:attributes].keys).to eq(%i[title url image country])
+      expect(learning_resource1[:data][:id]).to be_nil
+      expect(learning_resource1[:data][:type]).to eq 'learning_resource'
+      expect(learning_resource1[:data][:attributes]).to be_a Hash
+
+      attributes = learning_resource1[:data][:attributes]
+
+      expect(attributes[:country]).to eq('laos')
+      expect(attributes[:video][:title]).to be_a String
+      expect(attributes[:video][:youtube_video_id]).to be_a String
+
+      image1 = attributes[:images][0]
+      expect(image1[:alt_tag]).to be_a String
+      expect(image1[:url]).to be_a String
+    end
+  end
+
+  describe 'sad path' do
+    it 'returns empty array and hash if no images or videos', :vcr do
+      get api_v1_learning_resources_path({ country: 'asdfsafwefwefrbv333346g' })
+      learning_resource1 = JSON.parse(response.body, symbolize_names: true)
+      expect(learning_resource1[:data][:attributes][:images]).to eq []
+      expect(learning_resource1[:data][:attributes][:video]).to eq({})
     end
   end
 end
