beec35a5ff1239c447ed27c5121628cf76807914
Jake Kim <imjakekim@gmail.com>
2022-11-15 21:44:49 -0800

    rough recipe controller

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::RecipesController < ApplicationController
   def index
+    if params[:country]
+      response = RecipeFacade.search(params[:country])
+    end
+    render json: RecipeSerializer.new(response)
   end
 end
