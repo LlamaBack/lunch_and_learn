bb59732cc9ec5e6a2d156a7d6298b732378ac006
Jake Kim <imjakekim@gmail.com>
2022-11-15 21:55:52 -0800

    tested country service

--- app/facades/country_facade.rb
+++ app/facades/country_facade.rb
@@ -2,4 +2,10 @@ class CountryFacade
   def self.capital(country)
     CountryService.capital(country)[0][:capitalInfo][:latlng]
   end
+
+  def self.random
+    CountryService.all_countries.map do |country|
+      country[:name][:common]
+    end.sample
+  end
 end
--- app/services/country_service.rb
+++ app/services/country_service.rb
@@ -4,6 +4,11 @@ class CountryService
     JSON.parse(response.body, symbolize_names: true)
   end
 
+  def self.all_countries
+    response = conn.get('/v3.1/all')
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
   private
 
   def self.conn
